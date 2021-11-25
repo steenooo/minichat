94ec2d1ba7c295d5e6100cd53396c9bf137e31f4
Frederick Steen <steenooo5@gmail.com>
2021-11-25 22:43:16 +0100

    Added message, better support for prefixes etc

--- src/main/java/dev/steyn/minichat/MiniChatRenderer.java
+++ src/main/java/dev/steyn/minichat/MiniChatRenderer.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 import java.util.Optional;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.Template;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
@@ -29,22 +30,30 @@ public class MiniChatRenderer implements ChatRenderer {
     @Override
     public @NotNull Component render(@NotNull Player source, @NotNull Component sourceDisplayName,
         @NotNull Component message, @NotNull Audience viewer) {
-
         CachedMetaData metaData = Objects.requireNonNull(
                 luckPerms.getUserManager().getUser(source.getUniqueId())).getCachedData()
             .getMetaData();
         String format = metaData.getMetaValue("format");
         if(format == null) {
-            return Component.join(Component.text(": "), sourceDisplayName, message);
+            return Component.join(Component.text(": "), sourceDisplayName, message).color(
+                NamedTextColor.GRAY);
         }
-        Component prefix = Optional.ofNullable(metaData.getPrefix()).map(SERIALIZER::deserialize).orElse(Component.empty());
-        Component suffix = Optional.ofNullable(metaData.getSuffix()).map(SERIALIZER::deserialize).orElse(Component.empty());
+        Component prefix = Optional.ofNullable(metaData.getPrefix()).map(this::parse).orElse(Component.empty());
+        Component suffix = Optional.ofNullable(metaData.getSuffix()).map(this::parse).orElse(Component.empty());
 
         return MINI_MESSAGE.parse(format,
             Arrays.asList(
                 Template.of("name", sourceDisplayName),
                 Template.of("prefix", prefix),
-                Template.of("suffix", suffix)
+                Template.of("suffix", suffix),
+                Template.of("message", message)
             ));
+    }
+
+    private Component parse(String input) {
+        if(input.contains("&")) {
+            return SERIALIZER.deserialize(input);
+        }
+        return MINI_MESSAGE.parse(input);
     }
 }
